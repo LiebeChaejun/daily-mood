f25acbc4a46e7c5b15fbfa5d4669322fe121f0a1
Liebe_Chaeso <d131215@naver.com>
2026-06-21 15:26:26 +0900

    Home: 홈 페이지 Header 컴포넌트 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import "./Header.css";
 
-const Header = ({ text, leftChild, rightChild }) => {
+const Header = ({ title, leftChild, rightChild }) => {
   return (
     <header className="Header">
       <div className="header_left">{leftChild}</div>
-      <div className="header_title">{text}</div>
+      <div className="header_title">{title}</div>
       <div className="header_right">{rightChild}</div>
     </header>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,31 @@
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../../App";
+import Header from "../../components/Header/Header";
+import Button from "../../components/Button/Button";
+
 const Home = () => {
-  return <div>Home 페이지 입니다.</div>;
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const headerTitle = `${pivotDate.getFullYear()}년 ${
+    pivotDate.getMonth() + 1
+  }월`;
+
+  const onIncreaseDate = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
+  const onDecreaseDate = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  return (
+    <div>
+      <Header
+        title={headerTitle}
+        leftChild={<Button text={"<"} onClick={onDecreaseDate} />}
+        rightChild={<Button text={">"} onClick={onIncreaseDate} />}
+      />
+    </div>
+  );
 };
 
 export default Home;
