ef9244ef288b827f6f65d6ceac747a4fdd37c742
Liebe_Chaeso <d131215@naver.com>
2026-06-21 15:55:52 +0900

    Home: 홈 페이지 월간 데이터 필터링 기능

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,29 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { DiaryStateContext } from "../../App";
+import { getMonthlyDate } from "../../util/getFormattedDate";
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 
 const Home = () => {
+  const data = useContext(DiaryStateContext);
   const [pivotDate, setPivotDate] = useState(new Date());
+  const [filteredData, setFilteredData] = useState([]);
   const headerTitle = `${pivotDate.getFullYear()}년 ${
     pivotDate.getMonth() + 1
   }월`;
+
+  useEffect(() => {
+    if (data.length >= 1) {
+      const { beginTimeStamp, endTimeStamp } = getMonthlyDate(pivotDate);
+      setFilteredData(
+        data.filter(
+          (item) => beginTimeStamp <= item.date && item.date <= endTimeStamp
+        )
+      );
+    } else {
+      setFilteredData([]);
+    }
+  }, [data, pivotDate]);
 
   const onIncreaseDate = () => {
     setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
--- src/util/getFormattedDate.js
+++ src/util/getFormattedDate.js
@@ -12,3 +12,22 @@ export const getFormattedDate = (targetDate) => {
 
   return `${year}-${month}-${date}`;
 };
+
+export const getMonthlyDate = (date) => {
+  const beginTimeStamp = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    1
+  ).getTime();
+
+  const endTimeStamp = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  );
+
+  return { beginTimeStamp, endTimeStamp };
+};
