17093e86bf6926044e00e08c6b25b659e552f4a2
Liebe_Chaeso <d131215@naver.com>
2026-06-22 14:49:41 +0900

    Diary: Diary Header 파트 구현

--- src/pages/Diary/Diary.jsx
+++ src/pages/Diary/Diary.jsx
@@ -1,11 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useDiary from "../../hooks/useDiary";
+import Header from "../../components/Header/Header";
+import Button from "../../components/Button/Button";
+import { getFormattedDate } from "../../util/getFormattedDate";
 
 const Diary = () => {
   const { id } = useParams();
   const data = useDiary(id);
+  const nav = useNavigate();
 
-  return <div>Diary 페이지 입니다.</div>;
+  if (!data) {
+    return <div>일기를 불러오고 있습니다...</div>;
+  } else {
+    const { date, emotionId, content } = data;
+    const title = `${getFormattedDate(new Date(Number(date)))} 기록`;
+
+    return (
+      <div>
+        <Header
+          title={title}
+          leftChild={<Button text={"뒤로 가기"} onClick={() => nav(-1)} />}
+          rightChild={
+            <Button text={"수정하기"} onClick={() => nav(`/edit/${id}`)} />
+          }
+        />
+        <div>Diary 페이지입니다.</div>
+      </div>
+    );
+  }
 };
 
 export default Diary;
